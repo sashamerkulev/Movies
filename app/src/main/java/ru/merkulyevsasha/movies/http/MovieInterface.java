c7475b009193c5a9ff3d6494eba1ca2808862f26
Александр <a_lexandr@mail.ru>
2016-11-01 10:55:40 +0400

    Update MovieInterface.java

--- app/src/main/java/ru/merkulyevsasha/movies/http/MovieInterface.java
+++ app/src/main/java/ru/merkulyevsasha/movies/http/MovieInterface.java
@@ -1,9 +1,9 @@
 package ru.merkulyevsasha.movies.http;
 
-import retrofit.Call;
-import retrofit.http.GET;
-import retrofit.http.Path;
-import retrofit.http.Query;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 import ru.merkulyevsasha.movies.models.Details;
 import ru.merkulyevsasha.movies.models.Movies;
